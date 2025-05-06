e3f6a717f2697fd5530744ca6bc4e250672e57ff
21212078 <2121312078@uezo.edu.br>
2025-05-06 15:41:24 -0300

    Update zoologico.h
    Modificando para melhor entendimento

--- Zoologico/src/zoologico.h
+++ Zoologico/src/zoologico.h
@@ -1,5 +1,5 @@
-#ifndef ZOOLOGICO_H
-#define ZOOLOGICO_H
+#ifndef Zoologico_h
+#define Zoologico_h
 
 // Função que calcula o preço total dos bilhetes
 float calcular_preco(int idade, int quantidade);
